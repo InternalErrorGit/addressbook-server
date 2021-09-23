2ff5965caf8e35217001eeec36562565aa19cc7e
InternalErrorGit <phil.gatzka@gmail.com>
2021-09-23 16:24:43 +0200

    Created User-Service

---
+++ src/main/java/ch/zli/m223/addressbook/service/UserService.java
@@ -0,0 +1,45 @@
+package ch.zli.m223.addressbook.service;
+
+import ch.zli.m223.addressbook.entity.User;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.util.List;
+
+/**
+ * @author P. Gatzka
+ * @version 23.09.2021
+ * Project: addressbookserver
+ */
+@ApplicationScoped
+public class UserService extends AbstractService<User> {
+
+    @Inject
+    EntityManager entityManager;
+
+    @Override
+    @Transactional
+    public User create(User entity) {
+        entityManager.persist(entity);
+        return entity;
+    }
+
+    @Override
+    @Transactional
+    public void delete(Long id) {
+        entityManager.remove(entityManager.find(User.class, id));
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> findAll() {
+        return entityManager.createQuery("SELECT u FROM User u").getResultList();
+    }
+
+    @Override
+    public User find(Long id) {
+        return entityManager.find(User.class, id);
+    }
+}
