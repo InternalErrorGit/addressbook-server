9db07de70ef4ad7f1b3daa4dd6eaa616c648aba5
InternalErrorGit <phil.gatzka@gmail.com>
2021-09-23 16:25:10 +0200

    Created Address-Service

---
+++ src/main/java/ch/zli/m223/addressbook/service/AddressService.java
@@ -0,0 +1,45 @@
+package ch.zli.m223.addressbook.service;
+
+import ch.zli.m223.addressbook.entity.Address;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.util.List;
+
+/**
+ * @author P. Gatzka
+ * @version 23.09.2021
+ * Project: addressbookserver
+ */
+@ApplicationScoped
+public class AddressService extends AbstractService<Address> {
+
+    @Inject
+    EntityManager entityManager;
+
+    @Override
+    @Transactional
+    public Address create(Address entity) {
+        entityManager.persist(entity);
+        return entity;
+    }
+
+    @Override
+    @Transactional
+    public void delete(Long id) {
+        entityManager.remove(entityManager.find(Address.class, id));
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<Address> findAll() {
+        return entityManager.createQuery("SELECT a FROM Address a").getResultList();
+    }
+
+    @Override
+    public Address find(Long id) {
+        return entityManager.find(Address.class, id);
+    }
+}
